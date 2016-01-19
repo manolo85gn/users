275c0ed0159d9d32edbab93061e8908b6ac4e846
manuel <manolo85gn@gmail.com>
2016-01-19 11:27:32 -0600

    added mongosse configuration

--- server/app.js
+++ server/app.js
@@ -4,11 +4,15 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
   
 var express = require('express');
-var config = require('./config');
+var mongoose = require('mongoose');
+var config = require('./config/environment');
    
+// DB connection and config
+require('./config/db')(config);
+
 // Setup server
 var app = express();
-var http = require('http');
+var server = require('http').createServer(app);
     
 // Express configuration
 require('./config/express')(app);
@@ -16,7 +20,7 @@ require('./config/express')(app);
 require('./routes')(app);
      
 // Start server
-http.createServer(app).listen(config.port, function () {
+server.listen(config.port, config.ip, function () {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
        
